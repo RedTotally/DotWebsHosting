473231cdb3df2b246e5f9c9194c9b082afaf112f
TheRed <94051573+RedTotally@users.noreply.github.com>
2024-09-29 20:55:00 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import ClientLayout from "./ClientLayout";
-import Home from "./page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",

2e00db83703f16c30e779e283c60bce661b82f93
TheRed <94051573+RedTotally@users.noreply.github.com>
2025-01-08 17:46:56 +0800

    Update layout.tsx

--- src/app/(auth)/register/layout.tsx
+++ src/app/(auth)/register/layout.tsx
@@ -142,7 +142,7 @@ export default function Register() {
         Fixed_Username: username,
         Password: password,
         Code: generatedCookieCode,
-        Verified: false,
+        Verified: true,
         Product: "DotWebsHosting",
         Verification_Code: generatedCode,
         Password_Reset_Code: generatedCode_2,
